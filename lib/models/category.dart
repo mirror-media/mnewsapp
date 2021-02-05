f6fff299922544a5dcf5ca5c48483761a7916e0f
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-02-05 14:02:05 +0800

    add category and category list class

---
+++ lib/models/category.dart
@@ -0,0 +1,34 @@
+class Category {
+  String id;
+  String name;
+  String slug;
+
+  Category({
+    this.id,
+    this.name,
+    this.slug,
+  });
+
+  factory Category.fromJson(Map<String, dynamic> json) {
+    return Category(
+      id: json['id'],
+      name: json['name'],
+      slug: json['slug'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        '_id': id,
+        'name': name,
+        'slug': slug,
+      };
+
+  @override
+  int get hashCode => this.hashCode;
+
+  @override
+  bool operator ==(covariant Category other) {
+    // compare this to other
+    return this.id == other.id;
+  }
+}
---
+++ lib/models/categoryList.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:tv/models/category.dart';
+import 'package:tv/models/customizedList.dart';
+
+class CategoryList extends CustomizedList<Category> {
+  // constructor
+  CategoryList();
+
+  factory CategoryList.fromJson(List<dynamic> parsedJson) {
+    if (parsedJson == null) {
+      return null;
+    }
+
+    CategoryList categories = CategoryList();
+    List parseList = parsedJson.map((i) => Category.fromJson(i)).toList();
+    parseList.forEach((element) {
+      categories.add(element);
+    });
+
+    return categories;
+  }
+
+  factory CategoryList.parseResponseBody(String body) {
+    final jsonData = json.decode(body);
+
+    return CategoryList.fromJson(jsonData);
+  }
+
+  // your custom methods
+  List<Map<dynamic, dynamic>> toJson() {
+    List<Map> categoryMaps = List();
+    if (l == null) {
+      return null;
+    }
+
+    for (Category category in l) {
+      categoryMaps.add(category.toJson());
+    }
+    return categoryMaps;
+  }
+}
