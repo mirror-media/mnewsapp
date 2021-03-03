9745c254f906c9c663c4a567cf076a29793d8d2d
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-03-03 16:30:58 +0800

    refactor tab story list bloc

--- lib/blocs/tabStoryList/bloc.dart
+++ lib/blocs/tabStoryList/bloc.dart
@@ -16,31 +16,9 @@ class TabStoryListBloc extends Bloc<TabStoryListEvents, TabStoryListState> {
   @override
   Stream<TabStoryListState> mapEventToState(TabStoryListEvents event) async* {
     try {
-      if(event is FetchStoryList) {
-        yield TabStoryListLoading();
-        storyListItemList = await tabStoryListRepos.fetchStoryList();
-        yield TabStoryListLoaded(storyListItemList: storyListItemList);
-      } else if(event is FetchNextPage) {
-        yield TabStoryListLoadingMore(storyListItemList: storyListItemList);
-        StoryListItemList newStoryListItemList = await tabStoryListRepos.fetchNextPage(
-          loadingMorePage: event.loadingMorePage
-        );
-        storyListItemList.addAll(newStoryListItemList);
-        yield TabStoryListLoaded(storyListItemList: storyListItemList);
-      } else if(event is FetchStoryListByCategorySlug) {
-        yield TabStoryListLoading();
-        storyListItemList = await tabStoryListRepos.fetchStoryListByCategorySlug(event.slug);
-        yield TabStoryListLoaded(storyListItemList: storyListItemList);
-      } else if(event is FetchNextPageByCategorySlug) {
-        yield TabStoryListLoadingMore(storyListItemList: storyListItemList);
-        StoryListItemList newStoryListItemList = await tabStoryListRepos.fetchNextPageByCategorySlug(
-          event.slug, 
-          loadingMorePage: event.loadingMorePage
-        );
-        await Future.delayed(Duration(seconds: 3));
-        storyListItemList.addAll(newStoryListItemList);
-        yield TabStoryListLoaded(storyListItemList: storyListItemList);
-      }
+      event.storyListItemList = storyListItemList;
+      yield* event.run(tabStoryListRepos);
+      storyListItemList = event.storyListItemList;
     } on SocketException {
       yield TabStoryListError(
         error: NoInternetException('No Internet'),
