fa2139f77466903ce800b3b00da9f775b3b143ba
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-03-18 06:52:00 +0800

    add section pages in home page

---
+++ lib/pages/section/anchorpersonPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AnchorpersonPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Anchorperson'),
+    );
+  }
+}
---
+++ lib/pages/section/newsPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tv/blocs/categories/bloc.dart';
+import 'package:tv/services/categoryService.dart';
+import 'package:tv/widgets/categoryTab.dart';
+
+class NewsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CategoriesBloc(categoryRepos: CategoryServices()),
+      child: CategoryTab(),
+    );
+  }
+}
