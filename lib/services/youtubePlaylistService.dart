f228f0fc22827d7ce3f6af3b3bdc6fad78783f8a
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-04-08 15:36:54 +0800

    add max results in fetch snippet by playlist id api

--- lib/services/youtubePlaylistService.dart
+++ lib/services/youtubePlaylistService.dart
@@ -3,16 +3,19 @@ import 'package:tv/helpers/apiConstants.dart';
 import 'package:tv/models/youtubePlaylistItemList.dart';
 
 abstract class YoutubePlaylistRepos {
-  Future<YoutubePlaylistItemList> fetchSnippetByPlaylistId(String playlistId);
+  Future<YoutubePlaylistItemList> fetchSnippetByPlaylistId(
+    String playlistId, 
+    {int maxResults = 5}
+  );
 }
 
 class YoutubePlaylistServices implements YoutubePlaylistRepos{
   ApiBaseHelper _helper = ApiBaseHelper();
   
   @override
-  Future<YoutubePlaylistItemList> fetchSnippetByPlaylistId(String playlistId) async{
+  Future<YoutubePlaylistItemList> fetchSnippetByPlaylistId(String playlistId, {int maxResults = 5}) async{
     final jsonResponse = await _helper.getByUrl(
-      youtubeApi+'/playlistItems?part=snippet&playlistId=$playlistId'
+      youtubeApi+'/playlistItems?part=snippet&playlistId=$playlistId&maxResults=$maxResults'
     );
 
     YoutubePlaylistItemList youtubePlaylistItemList = YoutubePlaylistItemList.fromJson(
