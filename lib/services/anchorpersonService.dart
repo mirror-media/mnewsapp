13b73553b2a18d0252dd3f4d598831375efcb76b
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-04-12 16:59:03 +0800

    add fetch anchorperson by id api in anchorperson services

--- lib/services/anchorpersonService.dart
+++ lib/services/anchorpersonService.dart
@@ -2,10 +2,12 @@ import 'dart:convert';
 
 import 'package:tv/helpers/apiBaseHelper.dart';
 import 'package:tv/helpers/apiConstants.dart';
+import 'package:tv/models/anchorperson.dart';
 import 'package:tv/models/anchorpersonList.dart';
 import 'package:tv/models/graphqlBody.dart';
 
 abstract class AnchorpersonRepos {
+  Future<Anchorperson> fetchAnchorpersonById(String anchorpersonId);
   Future<AnchorpersonList> fetchAnchorpersonList();
 }
 
@@ -50,5 +52,47 @@ class AnchorpersonServices implements AnchorpersonRepos{
 
     AnchorpersonList anchorpersonList = AnchorpersonList.fromJson(jsonResponse['data']['allContacts']);
     return anchorpersonList;
+  }
+
+  @override
+  Future<Anchorperson> fetchAnchorpersonById(String anchorpersonId) async{
+    String query = 
+    """
+    query(\$where: ContactWhereUniqueInput!) {
+      Contact(where: \$where) {
+        name
+        image {
+          urlMobileSized
+        }
+        twitter
+        facebook
+        instatgram
+        bio
+      }
+    }
+    """;
+
+    Map<String,dynamic> variables = {
+      "where": {
+        "id": anchorpersonId
+      }
+    };
+
+    GraphqlBody graphqlBody = GraphqlBody(
+      operationName: null,
+      query: query,
+      variables: variables,
+    );
+
+    final jsonResponse = await _helper.postByUrl(
+      graphqlApi,
+      jsonEncode(graphqlBody.toJson()),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    );
+
+    Anchorperson anchorperson = Anchorperson.fromJson(jsonResponse['data']['Contact']);
+    return anchorperson;
   }
 }
