28d6cac6c7b8ce525dcdb52251839f3b3856df9e
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-04-13 21:33:34 +0800

    add story title and authors in story widget

--- lib/widgets/storyWidget.dart
+++ lib/widgets/storyWidget.dart
@@ -6,6 +6,8 @@ import 'package:tv/blocs/story/bloc.dart';
 import 'package:tv/blocs/story/states.dart';
 import 'package:tv/helpers/dataConstants.dart';
 import 'package:tv/helpers/dateTimeFormat.dart';
+import 'package:tv/models/people.dart';
+import 'package:tv/models/peopleList.dart';
 import 'package:tv/models/story.dart';
 import 'package:tv/widgets/story/mNewsVideoPlayer.dart';
 import 'package:tv/widgets/story/youtubeViewer.dart';
@@ -74,6 +76,10 @@ class _StoryWidgetState extends State<StoryWidget> {
         _buildHeroWidget(width, story),
         SizedBox(height: 24),
         _buildCategoryAndPublishedDate(story),
+        SizedBox(height: 10),
+        _buildStoryTitle(story.name),
+        SizedBox(height: 8),
+        _buildAuthors(story),
       ],
     );
   }
@@ -161,5 +167,177 @@ class _StoryWidgetState extends State<StoryWidget> {
         ],
       ),
     );
+  }
+  
+  Widget _buildStoryTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(24.0, 0.0, 24.0, 0.0),
+      child: Text(
+        title,
+        style: TextStyle(fontFamily: 'PingFang TC', fontSize: 26),
+      ),
+    );
+  }
+
+  Widget _buildAuthors(Story story) {
+    Color authorColor = Color(0xff757575);
+    List<Widget> authorItems = List();
+
+    // VerticalDivider is broken? so use Container
+    var myVerticalDivider = Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+      child: Container(
+        color: Color(0xff757575),
+        width: 1,
+        height: 15,
+      ),
+    );
+
+    if (story.writers.length > 0) {
+      authorItems.add(
+        Text(
+          "作者",
+          style: TextStyle(
+            fontSize: 15, 
+            color: authorColor
+          ),
+        ),
+      );
+      authorItems.add(myVerticalDivider);
+
+      authorItems.addAll(_addAuthorItems(story.writers));
+      authorItems.add(SizedBox(
+        width: 12.0,
+      ));
+    }
+
+    if (story.photographers.length > 0) {
+      authorItems.add(
+        Text(
+          "攝影",
+          style: TextStyle(
+            fontSize: 15, 
+            color: authorColor
+          ),
+        ),
+      );
+      authorItems.add(myVerticalDivider);
+
+      authorItems.addAll(_addAuthorItems(story.photographers));
+      authorItems.add(SizedBox(
+        width: 12.0,
+      ));
+    }
+
+    if (story.cameraOperators.length > 0) {
+      authorItems.add(
+        Text(
+          "影音",
+          style: TextStyle(
+            fontSize: 15, 
+            color: authorColor
+          ),
+        ),
+      );
+      authorItems.add(myVerticalDivider);
+
+      authorItems.addAll(_addAuthorItems(story.cameraOperators));
+      authorItems.add(SizedBox(
+        width: 12.0,
+      ));
+    }
+
+    if (story.designers.length > 0) {
+      authorItems.add(
+        Text(
+          "設計",
+          style: TextStyle(
+            fontSize: 15, 
+            color: authorColor
+          ),
+        ),
+      );
+      authorItems.add(myVerticalDivider);
+
+      authorItems.addAll(_addAuthorItems(story.designers));
+      authorItems.add(SizedBox(
+        width: 12.0,
+      ));
+    }
+
+    if (story.engineers.length > 0) {
+      authorItems.add(
+        Text(
+          "工程",
+          style: TextStyle(
+            fontSize: 15, 
+            color: authorColor
+          ),
+        ),
+      );
+      authorItems.add(myVerticalDivider);
+
+      authorItems.addAll(_addAuthorItems(story.engineers));
+      authorItems.add(SizedBox(
+        width: 12.0,
+      ));
+    }
+
+    if (story.vocals.length > 0) {
+      authorItems.add(
+        Text(
+          "主播",
+          style: TextStyle(
+            fontSize: 15, 
+            color: authorColor
+          ),
+        ),
+      );
+      authorItems.add(myVerticalDivider);
+
+      authorItems.addAll(_addAuthorItems(story.engineers));
+      authorItems.add(SizedBox(
+        width: 12.0,
+      ));
+    }
+
+    if (!_isNullOrEmpty(story.otherbyline)) {
+      authorItems.add(
+        Text(
+          "作者",
+          style: TextStyle(
+            fontSize: 15, 
+            color: authorColor
+          ),
+        ),
+      );
+      authorItems.add(myVerticalDivider);
+      authorItems.add(Text(story.otherbyline));
+    }
+
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(24.0, 0.0, 24.0, 0.0),
+      child: Wrap(
+        crossAxisAlignment: WrapCrossAlignment.center,
+        children: authorItems,
+      ),
+    );
+  }
+
+  List<Widget> _addAuthorItems(PeopleList peopleList) {
+    List<Widget> authorItems = List();
+
+    for (People author in peopleList) {
+      authorItems.add(Padding(
+        padding: const EdgeInsets.only(right: 4.0),
+        child: Text(
+          author.name,
+          style: TextStyle(
+            fontSize: 15, 
+          ),
+        ),
+      ));
+    }
+    return authorItems;
   }
 }
