4184414016114e4529cf62f0e38d3f2f25d8db41
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-04-13 17:00:45 +0800

    add fetch published story by slug api in story services

---
+++ lib/services/storyService.dart
@@ -0,0 +1,122 @@
+import 'dart:convert';
+
+import 'package:tv/helpers/apiBaseHelper.dart';
+import 'package:tv/helpers/apiConstants.dart';
+import 'package:tv/models/graphqlBody.dart';
+import 'package:tv/models/story.dart';
+
+abstract class StoryRepos {
+  Future<Story> fetchPublishedStoryBySlug(String slug);
+}
+
+class StoryServices implements StoryRepos{
+  ApiBaseHelper _helper = ApiBaseHelper();
+  
+  @override
+  Future<Story> fetchPublishedStoryBySlug(String slug) async{
+    final String query = 
+    """
+    query (
+      \$where: PostWhereInput,
+    ) {
+      allPosts(
+        where: \$where
+      ) {
+        style
+        name
+        brief
+        contentApiData
+        publishTime
+        updatedAt
+        heroImage {
+          mobile: urlMobileSized
+          desktop: urlDesktopSized
+        }
+        heroVideo {
+          coverPhoto {
+            tiny: urlTinySized
+            mobile: urlMobileSized
+            tablet: urlTabletSized
+            desktop: urlDesktopSized
+            original: urlOriginal
+          }
+          file {
+            publicUrl
+          }
+          url
+        }
+        heroCaption
+        categories {
+          slug
+          name
+        }
+        writers {
+          name
+          slug
+        }
+        photographers {
+          name
+          slug
+        }
+        cameraOperators {
+          name
+          slug
+        }
+        designers {
+          name
+          slug
+        }
+        engineers {
+          name 
+          slug
+        }
+        vocals {
+          name
+          slug
+        }
+        otherbyline
+        tags {
+          name
+        }
+        relatedPosts {
+          slug
+          name
+          heroImage {
+            urlMobileSized
+          }
+        }
+      }
+    }
+    """;
+
+    Map<String, dynamic> variables = {
+      "where": {
+        "state": "published",
+        "slug": slug
+      },
+    };
+
+    GraphqlBody graphqlBody = GraphqlBody(
+      operationName: null,
+      query: query,
+      variables: variables,
+    );
+
+    final jsonResponse = await _helper.postByUrl(
+      graphqlApi,
+      jsonEncode(graphqlBody.toJson()),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    );
+    
+    Story story;
+    try {
+      story = Story.fromJson(jsonResponse['data']['allPosts'][0]);
+    } catch(e) {
+      throw FormatException(e.toString()); 
+    }
+    
+    return story;
+  }
+}
