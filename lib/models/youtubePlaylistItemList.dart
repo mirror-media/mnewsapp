600f0b9f784f099903e330e05ca2feec33c75158
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-04-15 10:48:57 +0800

    add next page token parameter in youtube playlist item list class

--- lib/models/youtubePlaylistItemList.dart
+++ lib/models/youtubePlaylistItemList.dart
@@ -1,29 +1,22 @@
-import 'dart:convert';
-
 import 'package:tv/models/customizedList.dart';
 import 'package:tv/models/youtubePlaylistItem.dart';
 
 class YoutubePlaylistItemList extends CustomizedList<YoutubePlaylistItem> {
+  String nextPageToken;
   // constructor
-  YoutubePlaylistItemList();
+  YoutubePlaylistItemList(this.nextPageToken);
 
-  factory YoutubePlaylistItemList.fromJson(List<dynamic> parsedJson) {
+  factory YoutubePlaylistItemList.fromJson(String nextPageToken, List<dynamic> parsedJson) {
     if (parsedJson == null) {
       return null;
     }
 
-    YoutubePlaylistItemList youtubePlaylistItems = YoutubePlaylistItemList();
+    YoutubePlaylistItemList youtubePlaylistItems = YoutubePlaylistItemList(nextPageToken);
     List parseList = parsedJson.map((i) => YoutubePlaylistItem.fromJson(i)).toList();
     parseList.forEach((element) {
       youtubePlaylistItems.add(element);
     });
 
     return youtubePlaylistItems;
-  }
-
-  factory YoutubePlaylistItemList.parseResponseBody(String body) {
-    final jsonData = json.decode(body);
-
-    return YoutubePlaylistItemList.fromJson(jsonData);
   }
 }
