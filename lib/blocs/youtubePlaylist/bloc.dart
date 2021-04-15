e3b57cf0d27f0c833e4579f576d7bc6ba2c33509
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-04-15 11:09:41 +0800

    add loading more feature in youtube playlist bloc

--- lib/blocs/youtubePlaylist/bloc.dart
+++ lib/blocs/youtubePlaylist/bloc.dart
@@ -1,15 +1,19 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tv/blocs/youtubePlaylist/events.dart';
 import 'package:tv/blocs/youtubePlaylist/states.dart';
+import 'package:tv/models/youtubePlaylistItemList.dart';
 import 'package:tv/services/youtubePlaylistService.dart';
 
 class YoutubePlaylistBloc extends Bloc<YoutubePlaylistEvents, YoutubePlaylistState> {
   final YoutubePlaylistRepos youtubePlaylistRepos;
+  YoutubePlaylistItemList youtubePlaylistItemList;
 
   YoutubePlaylistBloc({this.youtubePlaylistRepos}) : super(YoutubePlaylistInitState());
 
   @override
   Stream<YoutubePlaylistState> mapEventToState(YoutubePlaylistEvents event) async* {
+    event.youtubePlaylistItemList = youtubePlaylistItemList;
     yield* event.run(youtubePlaylistRepos);
+    youtubePlaylistItemList = event.youtubePlaylistItemList;
   }
 }
