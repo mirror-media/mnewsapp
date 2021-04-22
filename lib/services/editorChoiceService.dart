4419470134cc274876511564edaad1c29c9ec660
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-04-23 07:36:39 +0800

    add fetching video editor chioce list api in editor choice services

--- lib/services/editorChoiceService.dart
+++ lib/services/editorChoiceService.dart
@@ -7,6 +7,7 @@ import 'package:tv/models/storyListItemList.dart';
 
 abstract class EditorChoiceRepos {
   Future<StoryListItemList> fetchEditorChoiceList();
+  Future<StoryListItemList> fetchVideoEditorChoiceList();
 }
 
 class EditorChoiceServices implements EditorChoiceRepos{
@@ -69,6 +70,70 @@ class EditorChoiceServices implements EditorChoiceRepos{
     List<dynamic> parsedJson = List<dynamic>();
     for(int i=0; i<jsonResponse['data']['allEditorChoices'].length; i++) {
       parsedJson.add(jsonResponse['data']['allEditorChoices'][i]['choice']);
+    }
+    StoryListItemList editorChoiceList = StoryListItemList.fromJson(parsedJson);    
+    return editorChoiceList;
+  }
+
+  @override
+  Future<StoryListItemList> fetchVideoEditorChoiceList() async{
+    String query = 
+    """
+    query(
+      \$where: VideoEditorChoiceWhereInput, 
+      \$first: Int
+    ){
+      allVideoEditorChoices(
+        where: \$where, 
+        first: \$first, 
+        sortBy: [order_ASC, createdAt_DESC]
+      ) {
+        videoEditor {
+          id
+          name
+          slug
+          style
+          heroImage {
+            urlMobileSized
+          }
+          heroVideo {
+            coverPhoto {
+              urlMobileSized
+            }
+          }
+        }
+      }
+    }
+    """;
+
+    Map<String, dynamic> variables = {
+      "where": {
+        "state": "published",
+        "videoEditor": {
+          "state": "published",
+          "style": "videoNews"
+        }
+      },
+      "first": 10
+    };
+
+    GraphqlBody graphqlBody = GraphqlBody(
+      operationName: null,
+      query: query,
+      variables: variables,
+    );
+
+    final jsonResponse = await _helper.postByUrl(
+      graphqlApi,
+      jsonEncode(graphqlBody.toJson()),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    );
+
+    List<dynamic> parsedJson = List<dynamic>();
+    for(int i=0; i<jsonResponse['data']['allVideoEditorChoices'].length; i++) {
+      parsedJson.add(jsonResponse['data']['allVideoEditorChoices'][i]['videoEditor']);
     }
     StoryListItemList editorChoiceList = StoryListItemList.fromJson(parsedJson);    
     return editorChoiceList;
