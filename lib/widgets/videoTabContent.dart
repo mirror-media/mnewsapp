2ea1c77fbd328720f3c3e701e5e03c147c755451
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-04-23 17:28:54 +0800

    add video tab story list in video tab content

--- lib/widgets/videoTabContent.dart
+++ lib/widgets/videoTabContent.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tv/blocs/editorChoice/bloc.dart';
 import 'package:tv/blocs/editorChoice/events.dart';
+import 'package:tv/blocs/tabStoryList/bloc.dart';
 import 'package:tv/services/editorChoiceService.dart';
+import 'package:tv/services/tabStoryListService.dart';
 import 'package:tv/widgets/editorChoiceStoryList.dart';
+import 'package:tv/widgets/videoTabStoryList.dart';
 
 class VideoTabContent extends StatefulWidget {
   final String categorySlug;
@@ -30,7 +33,12 @@ class _VideoTabContentState extends State<VideoTabContent> {
           ),
         
         if(!widget.needCarousel)
-          SliverToBoxAdapter(child: Center(child: Text(widget.categorySlug))),
+          BlocProvider(
+            create: (context) => TabStoryListBloc(tabStoryListRepos: TabStoryListServices(postStyle: 'videoNews')),
+            child: BuildVideoTabStoryList(
+              categorySlug: widget.categorySlug,
+            ),
+          ),
       ],
     );
   }
