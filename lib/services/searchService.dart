5de58038014d414aa667074198278e2f8527673c
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-05-25 15:05:56 +0800

    add search next page by keyword api

--- lib/services/searchService.dart
+++ lib/services/searchService.dart
@@ -6,13 +6,18 @@ import 'package:tv/models/storyListItemList.dart';
 
 abstract class SearchRepos {
   Future<StoryListItemList> searchNewsStoryByKeyword(String keyword, {int from = 0, int size = 20});
+  Future<StoryListItemList> searchNextPageByKeyword(String keyword, {int loadingMorePage = 20});
 }
 
 class SearchServices implements SearchRepos{
   ApiBaseHelper _helper = ApiBaseHelper();
-  
+  int from = 0, size = 20;
+
   @override
   Future<StoryListItemList> searchNewsStoryByKeyword(String keyword, {int from = 0, int size = 20}) async{
+    this.from = from;
+    this.size = size;
+
     var query = {
       "query": keyword,
       "from": from,
@@ -33,5 +38,12 @@ class SearchServices implements SearchRepos{
     storyListItemList.allStoryCount = jsonResponse["body"]["hits"]["total"]["value"];
     
     return storyListItemList;
+  }
+
+  @override
+  Future<StoryListItemList> searchNextPageByKeyword(String keyword, {int loadingMorePage = 20}) async{
+    from = from + size;
+    size = loadingMorePage;
+    return await searchNewsStoryByKeyword(keyword, from: from, size: size);
   }
 }
