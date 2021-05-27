525ca0d52175d9c107800528ec9183e08a822c5d
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-05-27 16:53:52 +0800

    add fetch all promotion videos api in promotion videos services

---
+++ lib/services/promotionVideosService.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+
+import 'package:tv/helpers/apiBaseHelper.dart';
+import 'package:tv/helpers/apiConstants.dart';
+import 'package:tv/models/graphqlBody.dart';
+import 'package:tv/models/youtubePlaylistItemList.dart';
+
+abstract class PromotionVideosRepos {
+  Future<YoutubePlaylistItemList> fetchAllPromotionVideos();
+}
+
+class PromotionVideosServices implements PromotionVideosRepos{
+  ApiBaseHelper _helper = ApiBaseHelper();
+
+  @override
+  Future<YoutubePlaylistItemList> fetchAllPromotionVideos() async{
+    final String query = 
+    """
+    query {
+      allPromotionVideos(
+        where: {
+          state: published
+        }
+        sortBy: [ sortOrder_ASC, updatedAt_DESC ]
+      ) {
+        ytUrl
+      }
+    }
+    """;
+
+    Map<String, dynamic> variables = {};
+
+    GraphqlBody graphqlBody = GraphqlBody(
+      operationName: null,
+      query: query,
+      variables: variables,
+    );
+
+    final jsonResponse = await _helper.postByUrl(
+      graphqlApi,
+      jsonEncode(graphqlBody.toJson()),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    );
+
+    YoutubePlaylistItemList youtubePlaylistItemList = YoutubePlaylistItemList.fromPromotionVideosJson(jsonResponse['data']['allPromotionVideos']);
+    return youtubePlaylistItemList;
+  }
+}
