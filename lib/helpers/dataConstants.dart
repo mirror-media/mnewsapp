4b9e57adff1a207286d544c424a3450399ae6550
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-06-09 19:20:03 +0800

    add some ombuds button icons

--- lib/helpers/dataConstants.dart
+++ lib/helpers/dataConstants.dart
@@ -15,6 +15,13 @@ const String error500Png = 'assets/image/error500.png';
 const String noSignalPng = 'assets/image/noSignal.png';
 const String searchNoResultPng = 'assets/image/search/searchNoResult.png';
 const String ombudsDefaultJpg = 'assets/image/ombuds/ombudsDefault.jpg';
+const String tvPng = 'assets/image/ombuds/tv.png';
+const String phonePng = 'assets/image/ombuds/phone.png';
+const String paperPng = 'assets/image/ombuds/paper.png';
+const String videoRecorderPng = 'assets/image/ombuds/videoRecorder.png';
+const String faqPng = 'assets/image/ombuds/FAQ.png';
+const String reportPng = 'assets/image/ombuds/report.png';
+const String mailPng = 'assets/image/ombuds/mail.png';
 
 /// color
 const Color themeColor = Color(0xff003366);
