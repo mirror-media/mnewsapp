fe3b29089a15c27b1f53cb9171afacb1afdc4bdb
samfeng0517 <sam0880517@gmail.com>
2021-08-30 16:29:18 +0800

    Add program list into page

--- lib/pages/section/programList/programListWidget.dart
+++ lib/pages/section/programList/programListWidget.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:tv/blocs/programList/program_list_cubit.dart';
+import 'package:tv/helpers/exceptions.dart';
+import 'package:tv/models/programList.dart';
+import 'package:tv/models/programListItem.dart';
 import 'package:tv/widgets/customPicker.dart';
 
 class ProgramListWidget extends StatefulWidget{
@@ -15,19 +20,53 @@ class _ProgramListWidgetState extends State<ProgramListWidget>{
   bool _isDefault = true;
 
   @override
+  void initState() {
+    _loadProgramList();
+    super.initState();
+  }
+
+  _loadProgramList() async {
+    context.read<ProgramListCubit>().fetchProgramList();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(20, 16, 20, 24),
-      child: Column(
-        children: [
-          _buildChooseButton(),
-          SizedBox(height: 16,),
-          _buildLabel(),
-          SizedBox(height: 8,),
-        ],
-      ),
+    return BlocBuilder<ProgramListCubit, ProgramListState>(
+      builder: (context,state){
+        if (state is ProgramListError) {
+          final error = state.error;
+          print('ProgramListError: ${error.message}');
+          if( error is NoInternetException) {
+            return error.renderWidget(onPressed: () => _loadProgramList());
+          }
+
+          return error.renderWidget(isNoButton: true);
+        }
+        if(state is ProgramListLoaded){
+          return Padding(
+            padding: EdgeInsets.fromLTRB(20, 16, 20, 24),
+            child: Column(
+              children: [
+                _buildChooseButton(),
+                SizedBox(height: 16,),
+                _buildLabel(),
+                SizedBox(height: 8,),
+                Expanded(
+                  child: _buildContent(state.programList),
+                )
+              ],
+            ),
+          );
+        }
+        return _loadingWidget();
+      },
     );
   }
+
+  Widget _loadingWidget() =>
+      Center(
+        child: CircularProgressIndicator(),
+      );
 
   Widget _buildChooseButton(){
     return OutlinedButton(
@@ -80,6 +119,100 @@ class _ProgramListWidgetState extends State<ProgramListWidget>{
         Text('時間',style: TextStyle(fontSize: 15),),
         Text('節目名稱',style: TextStyle(fontSize: 15),),
         Text('分級',style: TextStyle(fontSize: 15),),
+      ],
+    );
+  }
+
+  Widget _buildContent(ProgramList programList){
+    ProgramList _pickedProgramList = new ProgramList();
+    int start = programList.indexWhere(
+            (element) => element.year == _selectedDate.year && element.weekDay == _selectedDate.weekday
+    );
+    int end = programList.lastIndexWhere(
+            (element) => element.year == _selectedDate.year && element.weekDay == _selectedDate.weekday
+    );
+
+    for(int i = start; i <= end; i++){
+      _pickedProgramList.add(programList[i]);
+    }
+
+    if(end != programList.length - 1){
+      _pickedProgramList.add(programList[end+1]);
+    }
+    else{
+      if(_selectedDate.weekday == 7){
+        _pickedProgramList.add(
+            programList.firstWhere(
+                    (element) => element.weekDay == 1 && element.year == _selectedDate.year,
+              orElse: (){
+                return programList.firstWhere((element) => element.weekDay == 1);
+              }
+            )
+        );
+      }
+      else{
+        _pickedProgramList.add(
+            programList.firstWhere(
+                    (element) => element.weekDay == _selectedDate.weekday + 1 && element.year == _selectedDate.year,
+              orElse: (){
+                return programList.firstWhere((element) => element.weekDay == _selectedDate.weekday + 1);
+              }
+            ));
+      }
+    }
+
+    return ListView.separated(
+        itemBuilder: (context, index){
+          return _programListViewItem(_pickedProgramList[index],_pickedProgramList[index+1]);
+        },
+        separatorBuilder: (context, index){
+          return Divider(height: 16.5, thickness: 0.5, color: Colors.black,);
+        },
+        itemCount: _pickedProgramList.length - 1,
+    );
+  }
+
+  Widget _programListViewItem(ProgramListItem today, ProgramListItem tomorrow){
+    String startHour =
+    (today.startTimeHour < 10) ? '0${today.startTimeHour.toString()}' : today.startTimeHour.toString();
+    String startMinute =
+    (today.startTimeMinute < 10) ? '0${today.startTimeMinute.toString()}' : today.startTimeMinute.toString();
+    String endHour =
+    (tomorrow.startTimeHour < 10) ? '0${tomorrow.startTimeHour.toString()}' : tomorrow.startTimeHour.toString();
+    String endMinute =
+    (tomorrow.startTimeMinute < 10) ? '0${tomorrow.startTimeMinute.toString()}' : tomorrow.startTimeMinute.toString();
+    if(endHour == '00' && endMinute == '00')
+      endHour = '24';
+    String time = '$startHour:$startMinute-$endHour:$endMinute';
+
+    Widget newOrRepeat;
+    if(today.txCategory == 'Repeat')
+      newOrRepeat = Text('(重播)', style: TextStyle(fontSize: 15, color: Color(0xE5979797)),);
+    else
+      newOrRepeat = Text('(新播)', style: TextStyle(fontSize: 15),);
+    Widget name = Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Text(today.programme,
+              style: TextStyle(fontSize: 15),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(width: 5,),
+          newOrRepeat
+        ],
+      ),
+    );
+
+    return Row(
+      children: [
+        Text(time, style: TextStyle(fontSize: 15),),
+        SizedBox(width: 20,),
+        name,
+        SizedBox(width: 20,),
+        Text(today.showClass, style: TextStyle(fontSize: 15, color: Color(0xE5979797)),)
       ],
     );
   }
