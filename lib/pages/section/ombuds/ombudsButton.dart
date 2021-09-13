5587fe3aa08a3548e0b0a8696835531a894bd94e
samfeng0517 <sam0880517@gmail.com>
2021-09-13 14:24:15 +0800

    Change height of ombudsButton

--- lib/pages/section/ombuds/ombudsButton.dart
+++ lib/pages/section/ombuds/ombudsButton.dart
@@ -34,7 +34,7 @@ class OmbudsButton extends StatelessWidget {
           Container(
             width: width,
             color: Colors.white,
-            height: 84,
+            height: 90,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
