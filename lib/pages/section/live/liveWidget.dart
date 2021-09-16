a50a1df5f5da732d62323c4e80b5c0026b44a511
samfeng0517 <sam0880517@gmail.com>
2021-09-16 18:12:03 +0800

    Change live part to become a stateful widget

---
+++ lib/pages/section/live/liveWidget.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tv/baseConfig.dart';
+import 'package:tv/blocs/live/liveCubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tv/widgets/story/youtubeViewer.dart';
+
+class LiveWidget extends StatefulWidget {
+  @override
+  _LiveWidgetState createState() => _LiveWidgetState();
+}
+
+class _LiveWidgetState extends State<LiveWidget> {
+  @override
+  void initState() {
+    _loadLiveId();
+    super.initState();
+  }
+
+  _loadLiveId() {
+    context.read<LiveCubit>().fetchLiveId(baseConfig!.mNewsLivePostId);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LiveCubit, LiveState>(
+      builder: (BuildContext context, LiveState state) {
+        if (state is LiveIdLoaded) {
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(24, 24, 24, 12),
+                child: _buildLiveTitle('鏡電視 Live'),
+              ),
+              YoutubeViewer(
+                state.liveId,
+                autoPlay: true,
+                isLive: true,
+                mute: true,
+              ),
+            ],
+          );
+        }
+        return Container();
+      },
+    );
+  }
+
+  Widget _buildLiveTitle(String title) {
+    return Row(
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 17,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(width: 8.0),
+        FaIcon(
+          FontAwesomeIcons.podcast,
+          size: 18,
+          color: Colors.red,
+        ),
+      ],
+    );
+  }
+}
