f8cbca6eaeeebacb53107c4fdce49f1ad805e45e
samfeng0517 <sam0880517@gmail.com>
2021-09-30 14:11:55 +0800

    Beautify config page

--- lib/pages/configPage.dart
+++ lib/pages/configPage.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:tv/helpers/dataConstants.dart';
 
 class ConfigPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
+      backgroundColor: themeColor,
+      body: SizedBox.expand(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              logoPng,
+              scale: 4.0,
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
       ),
     );
   }
