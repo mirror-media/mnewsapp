ec768315eda27e323d031e2362c9bfd22e0c84a1
samfeng0517 <sam0880517@gmail.com>
2021-09-30 15:05:47 +0800

    Refactor notificationSetting bloc

--- lib/blocs/notificationSetting/bloc.dart
+++ lib/blocs/notificationSetting/bloc.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tv/blocs/notificationSetting/events.dart';
 import 'package:tv/blocs/notificationSetting/states.dart';
+import 'package:tv/helpers/exceptions.dart';
+import 'package:tv/models/notificationSettingList.dart';
 import 'package:tv/services/notificationSettingService.dart';
 
 class NotificationSettingBloc
@@ -13,6 +17,51 @@ class NotificationSettingBloc
   @override
   Stream<NotificationSettingState> mapEventToState(
       NotificationSettingEvents event) async* {
-    yield* event.run(notificationSettingRepos);
+    print(event.toString());
+    try {
+      if (event is GetNotificationSettingList) {
+        yield NotificationSettingLoading();
+        NotificationSettingList notificationSettingList =
+            await notificationSettingRepos.getNotificationSettingList();
+        yield NotificationSettingLoaded(
+            notificationSettingList: notificationSettingList);
+      } else if (event is NotificationOnExpansionChanged) {
+        NotificationSettingList notificationSettingList =
+            notificationSettingRepos.onExpansionChanged(
+          event.inputNotificationSettingList,
+          event.index,
+          event.value,
+        );
+        yield NotificationSettingLoaded(
+            notificationSettingList: notificationSettingList);
+      } else if (event is NotificationOnCheckBoxChanged) {
+        NotificationSettingList notificationSettingList =
+            notificationSettingRepos.onCheckBoxChanged(
+          event.inputNotificationSettingList,
+          event.checkboxList,
+          event.index,
+          event.isRepeatable,
+        );
+        yield NotificationSettingLoaded(
+            notificationSettingList: notificationSettingList);
+      }
+    } on SocketException {
+      yield NotificationSettingError(
+        error: NoInternetException('No Internet'),
+      );
+    } on HttpException {
+      yield NotificationSettingError(
+        error: NoServiceFoundException('No Service Found'),
+      );
+    } on FormatException {
+      yield NotificationSettingError(
+        error: InvalidFormatException('Invalid Response format'),
+      );
+    } catch (e) {
+      print('${event.toString()} error: $e');
+      yield NotificationSettingError(
+        error: UnknownException(e.toString()),
+      );
+    }
   }
 }
--- lib/blocs/notificationSetting/events.dart
+++ lib/blocs/notificationSetting/events.dart
@@ -1,49 +1,10 @@
-import 'dart:io';
-
-import 'package:tv/blocs/notificationSetting/states.dart';
-import 'package:tv/helpers/exceptions.dart';
 import 'package:tv/models/notificationSettingList.dart';
-import 'package:tv/services/notificationSettingService.dart';
 
-abstract class NotificationSettingEvents {
-  Stream<NotificationSettingState> run(
-      NotificationSettingRepos notificationSettingRepos);
-}
+abstract class NotificationSettingEvents {}
 
 class GetNotificationSettingList extends NotificationSettingEvents {
-  GetNotificationSettingList();
-
   @override
   String toString() => 'GetNotificationSettingList';
-
-  @override
-  Stream<NotificationSettingState> run(
-      NotificationSettingRepos notificationSettingRepos) async* {
-    print(this.toString());
-    try {
-      yield NotificationSettingLoading();
-      NotificationSettingList notificationSettingList =
-          await notificationSettingRepos.getNotificationSettingList();
-      yield NotificationSettingLoaded(
-          notificationSettingList: notificationSettingList);
-    } on SocketException {
-      yield NotificationSettingError(
-        error: NoInternetException('No Internet'),
-      );
-    } on HttpException {
-      yield NotificationSettingError(
-        error: NoServiceFoundException('No Service Found'),
-      );
-    } on FormatException {
-      yield NotificationSettingError(
-        error: InvalidFormatException('Invalid Response format'),
-      );
-    } catch (e) {
-      yield NotificationSettingError(
-        error: UnknownException(e.toString()),
-      );
-    }
-  }
 }
 
 class NotificationOnExpansionChanged extends NotificationSettingEvents {
@@ -55,24 +16,6 @@ class NotificationOnExpansionChanged extends NotificationSettingEvents {
 
   @override
   String toString() => 'NotificationOnExpansionChanged';
-
-  @override
-  Stream<NotificationSettingState> run(
-      NotificationSettingRepos notificationSettingRepos) async* {
-    print(this.toString());
-    try {
-      NotificationSettingList notificationSettingList =
-          notificationSettingRepos.onExpansionChanged(
-        inputNotificationSettingList,
-        index,
-        value,
-      );
-      yield NotificationSettingLoaded(
-          notificationSettingList: notificationSettingList);
-    } catch (e) {
-      print('NotificationOnExpansionChanged error: $e');
-    }
-  }
 }
 
 class NotificationOnCheckBoxChanged extends NotificationSettingEvents {
@@ -85,23 +28,4 @@ class NotificationOnCheckBoxChanged extends NotificationSettingEvents {
 
   @override
   String toString() => 'NotificationOnCheckBoxChanged';
-
-  @override
-  Stream<NotificationSettingState> run(
-      NotificationSettingRepos notificationSettingRepos) async* {
-    print(this.toString());
-    try {
-      NotificationSettingList notificationSettingList =
-          notificationSettingRepos.onCheckBoxChanged(
-        inputNotificationSettingList,
-        checkboxList,
-        index,
-        isRepeatable,
-      );
-      yield NotificationSettingLoaded(
-          notificationSettingList: notificationSettingList);
-    } catch (e) {
-      print('NotificationOnCheckBoxChanged error: $e');
-    }
-  }
 }
