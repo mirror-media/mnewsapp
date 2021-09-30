ec57631f77a6231df2db6c9560ce6dcb3019bfa0
samfeng0517 <sam0880517@gmail.com>
2021-09-30 17:55:04 +0800

    Refactor youtubePlaylist bloc

--- lib/blocs/youtubePlaylist/bloc.dart
+++ lib/blocs/youtubePlaylist/bloc.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:tv/blocs/youtubePlaylist/events.dart';
 import 'package:tv/blocs/youtubePlaylist/states.dart';
+import 'package:tv/helpers/exceptions.dart';
 import 'package:tv/models/youtubePlaylistItemList.dart';
 import 'package:tv/services/youtubePlaylistService.dart';
 
@@ -15,8 +20,59 @@ class YoutubePlaylistBloc
   @override
   Stream<YoutubePlaylistState> mapEventToState(
       YoutubePlaylistEvents event) async* {
-    event.youtubePlaylistItemList = youtubePlaylistItemList;
-    yield* event.run(youtubePlaylistRepos);
-    youtubePlaylistItemList = event.youtubePlaylistItemList;
+    print(event.toString());
+    try {
+      yield YoutubePlaylistLoading();
+      if (event is FetchSnippetByPlaylistId) {
+        youtubePlaylistItemList = await youtubePlaylistRepos
+            .fetchSnippetByPlaylistId(event.playlistId,
+                maxResults: event.maxResults);
+        yield YoutubePlaylistLoaded(
+            youtubePlaylistItemList: youtubePlaylistItemList);
+      } else if (event is FetchSnippetByPlaylistIdAndPageToken) {
+        yield YoutubePlaylistLoadingMore(
+            youtubePlaylistItemList: youtubePlaylistItemList);
+        YoutubePlaylistItemList newYoutubePlaylistItemList =
+            await youtubePlaylistRepos.fetchSnippetByPlaylistIdAndPageToken(
+                event.playlistId, event.pageToken,
+                maxResults: event.maxResults);
+        youtubePlaylistItemList.nextPageToken =
+            newYoutubePlaylistItemList.nextPageToken;
+        youtubePlaylistItemList.addAll(newYoutubePlaylistItemList);
+
+        yield YoutubePlaylistLoaded(
+            youtubePlaylistItemList: youtubePlaylistItemList);
+      }
+    } on SocketException {
+      yield YoutubePlaylistError(
+        error: NoInternetException('No Internet'),
+      );
+    } on HttpException {
+      yield YoutubePlaylistError(
+        error: NoServiceFoundException('No Service Found'),
+      );
+    } on FormatException {
+      yield YoutubePlaylistError(
+        error: InvalidFormatException('Invalid Response format'),
+      );
+    } catch (e) {
+      if (event is FetchSnippetByPlaylistIdAndPageToken) {
+        Fluttertoast.showToast(
+            msg: "加載失敗",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+        await Future.delayed(Duration(seconds: 5));
+        yield YoutubePlaylistLoadingMoreFail(
+            youtubePlaylistItemList: youtubePlaylistItemList);
+      } else {
+        yield YoutubePlaylistError(
+          error: UnknownException(e.toString()),
+        );
+      }
+    }
   }
 }
--- lib/blocs/youtubePlaylist/events.dart
+++ lib/blocs/youtubePlaylist/events.dart
@@ -1,16 +1,4 @@
-import 'dart:io';
-
-import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:tv/blocs/youtubePlaylist/states.dart';
-import 'package:tv/helpers/exceptions.dart';
-import 'package:tv/models/youtubePlaylistItemList.dart';
-import 'package:tv/services/youtubePlaylistService.dart';
-
-abstract class YoutubePlaylistEvents {
-  YoutubePlaylistItemList youtubePlaylistItemList = YoutubePlaylistItemList();
-  Stream<YoutubePlaylistState> run(YoutubePlaylistRepos youtubePlaylistRepos);
-}
+abstract class YoutubePlaylistEvents {}
 
 class FetchSnippetByPlaylistId extends YoutubePlaylistEvents {
   final String playlistId;
@@ -20,35 +8,6 @@ class FetchSnippetByPlaylistId extends YoutubePlaylistEvents {
   @override
   String toString() =>
       'FetchSnippetByPlaylistId { storySlug: $playlistId, maxResults: $maxResults }';
-
-  @override
-  Stream<YoutubePlaylistState> run(
-      YoutubePlaylistRepos youtubePlaylistRepos) async* {
-    print(this.toString());
-    try {
-      yield YoutubePlaylistLoading();
-      youtubePlaylistItemList = await youtubePlaylistRepos
-          .fetchSnippetByPlaylistId(playlistId, maxResults: maxResults);
-      yield YoutubePlaylistLoaded(
-          youtubePlaylistItemList: youtubePlaylistItemList);
-    } on SocketException {
-      yield YoutubePlaylistError(
-        error: NoInternetException('No Internet'),
-      );
-    } on HttpException {
-      yield YoutubePlaylistError(
-        error: NoServiceFoundException('No Service Found'),
-      );
-    } on FormatException {
-      yield YoutubePlaylistError(
-        error: InvalidFormatException('Invalid Response format'),
-      );
-    } catch (e) {
-      yield YoutubePlaylistError(
-        error: UnknownException(e.toString()),
-      );
-    }
-  }
 }
 
 class FetchSnippetByPlaylistIdAndPageToken extends YoutubePlaylistEvents {
@@ -61,36 +20,4 @@ class FetchSnippetByPlaylistIdAndPageToken extends YoutubePlaylistEvents {
   @override
   String toString() =>
       'FetchSnippetByPlaylistIdAndPageToken { playlistId: $playlistId, pageToken: $pageToken, maxResults: $maxResults }';
-
-  @override
-  Stream<YoutubePlaylistState> run(
-      YoutubePlaylistRepos youtubePlaylistRepos) async* {
-    print(this.toString());
-    try {
-      yield YoutubePlaylistLoadingMore(
-          youtubePlaylistItemList: youtubePlaylistItemList);
-      YoutubePlaylistItemList newYoutubePlaylistItemList =
-          await youtubePlaylistRepos.fetchSnippetByPlaylistIdAndPageToken(
-              playlistId, pageToken,
-              maxResults: maxResults);
-      youtubePlaylistItemList.nextPageToken =
-          newYoutubePlaylistItemList.nextPageToken;
-      youtubePlaylistItemList.addAll(newYoutubePlaylistItemList);
-
-      yield YoutubePlaylistLoaded(
-          youtubePlaylistItemList: youtubePlaylistItemList);
-    } catch (e) {
-      Fluttertoast.showToast(
-          msg: "加載失敗",
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.CENTER,
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.red,
-          textColor: Colors.white,
-          fontSize: 16.0);
-      await Future.delayed(Duration(seconds: 5));
-      yield YoutubePlaylistLoadingMoreFail(
-          youtubePlaylistItemList: youtubePlaylistItemList);
-    }
-  }
 }
