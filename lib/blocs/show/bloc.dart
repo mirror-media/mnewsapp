68fc550d703a471ea09a6ec5f57ca202e08b3d29
samfeng0517 <sam0880517@gmail.com>
2021-09-30 17:33:56 +0800

    Refactor show bloc

--- lib/blocs/show/bloc.dart
+++ lib/blocs/show/bloc.dart
@@ -1,6 +1,14 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tv/blocs/show/events.dart';
 import 'package:tv/blocs/show/states.dart';
+import 'package:tv/helpers/dataConstants.dart';
+import 'package:tv/helpers/exceptions.dart';
+import 'package:tv/models/adUnitId.dart';
+import 'package:tv/models/showIntro.dart';
 import 'package:tv/services/showService.dart';
 
 class ShowIntroBloc extends Bloc<ShowEvents, ShowState> {
@@ -10,6 +18,33 @@ class ShowIntroBloc extends Bloc<ShowEvents, ShowState> {
 
   @override
   Stream<ShowState> mapEventToState(ShowEvents event) async* {
-    yield* event.run(showRepos);
+    print(event.toString());
+    try {
+      yield ShowLoading();
+      if (event is FetchShowIntro) {
+        ShowIntro showIntro =
+            await showRepos.fetchShowIntroById(event.showCategoryId);
+        String jsonFixed = await rootBundle.loadString(adUnitIdJson);
+        final fixedAdUnitId = json.decode(jsonFixed);
+        AdUnitId adUnitId = AdUnitId.fromJson(fixedAdUnitId, 'show');
+        yield ShowIntroLoaded(showIntro: showIntro, adUnitId: adUnitId);
+      }
+    } on SocketException {
+      yield ShowError(
+        error: NoInternetException('No Internet'),
+      );
+    } on HttpException {
+      yield ShowError(
+        error: NoServiceFoundException('No Service Found'),
+      );
+    } on FormatException {
+      yield ShowError(
+        error: InvalidFormatException('Invalid Response format'),
+      );
+    } catch (e) {
+      yield ShowError(
+        error: UnknownException(e.toString()),
+      );
+    }
   }
 }
