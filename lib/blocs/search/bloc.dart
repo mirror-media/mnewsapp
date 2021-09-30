5f205c50a2e136ecc0b5f059193175ea4e17bd4f
samfeng0517 <sam0880517@gmail.com>
2021-09-30 17:29:17 +0800

    Refactor search bloc

--- lib/blocs/search/bloc.dart
+++ lib/blocs/search/bloc.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tv/blocs/search/events.dart';
 import 'package:tv/blocs/search/states.dart';
+import 'package:tv/helpers/apiException.dart';
+import 'package:tv/helpers/exceptions.dart';
 import 'package:tv/models/storyListItemList.dart';
 import 'package:tv/services/searchService.dart';
 
@@ -12,8 +16,58 @@ class SearchBloc extends Bloc<SearchEvents, SearchState> {
 
   @override
   Stream<SearchState> mapEventToState(SearchEvents event) async* {
-    event.storyListItemList = storyListItemList;
-    yield* event.run(searchRepos);
-    storyListItemList = event.storyListItemList;
+    print(event.toString());
+    try {
+      yield SearchLoading();
+      if (event is SearchNewsStoryByKeyword) {
+        storyListItemList =
+            await searchRepos.searchNewsStoryByKeyword(event.keyword);
+        yield SearchLoaded(storyListItemList: storyListItemList);
+      } else if (event is SearchNextPageByKeyword) {
+        yield SearchLoadingMore(storyListItemList: storyListItemList);
+        StoryListItemList newStoryListItemList =
+            await searchRepos.searchNextPageByKeyword(event.keyword);
+        storyListItemList.addAll(newStoryListItemList);
+        yield SearchLoaded(storyListItemList: storyListItemList);
+      } else if (event is ClearKeyword) {
+        yield SearchInitState();
+      }
+    } on SocketException {
+      yield SearchError(
+        error: NoInternetException('No Internet'),
+      );
+    } on HttpException {
+      yield SearchError(
+        error: NoServiceFoundException('No Service Found'),
+      );
+    } on FormatException {
+      yield SearchError(
+        error: InvalidFormatException('Invalid Response format'),
+      );
+    } on FetchDataException {
+      yield SearchError(
+        error: NoInternetException('Error During Communication'),
+      );
+    } on BadRequestException {
+      yield SearchError(
+        error: Error400Exception('Invalid Request'),
+      );
+    } on UnauthorisedException {
+      yield SearchError(
+        error: Error400Exception('Unauthorised'),
+      );
+    } on InvalidInputException {
+      yield SearchError(
+        error: Error400Exception('Invalid Input'),
+      );
+    } on InternalServerErrorException {
+      yield SearchError(
+        error: Error500Exception('Internal Server Error'),
+      );
+    } catch (e) {
+      yield SearchError(
+        error: UnknownException(e.toString()),
+      );
+    }
   }
 }
--- lib/blocs/search/events.dart
+++ lib/blocs/search/events.dart
@@ -1,15 +1,4 @@
-import 'dart:io';
-
-import 'package:tv/blocs/search/states.dart';
-import 'package:tv/helpers/apiException.dart';
-import 'package:tv/helpers/exceptions.dart';
-import 'package:tv/models/storyListItemList.dart';
-import 'package:tv/services/searchService.dart';
-
-abstract class SearchEvents {
-  StoryListItemList storyListItemList = StoryListItemList();
-  Stream<SearchState> run(SearchRepos searchRepos);
-}
+abstract class SearchEvents {}
 
 class SearchNewsStoryByKeyword extends SearchEvents {
   final String keyword;
@@ -17,52 +6,6 @@ class SearchNewsStoryByKeyword extends SearchEvents {
 
   @override
   String toString() => 'SearchNewsStoryByKeyword { keyword: $keyword }';
-
-  @override
-  Stream<SearchState> run(SearchRepos searchRepos) async* {
-    print(this.toString());
-    try {
-      yield SearchLoading();
-      storyListItemList = await searchRepos.searchNewsStoryByKeyword(keyword);
-      yield SearchLoaded(storyListItemList: storyListItemList);
-    } on SocketException {
-      yield SearchError(
-        error: NoInternetException('No Internet'),
-      );
-    } on HttpException {
-      yield SearchError(
-        error: NoServiceFoundException('No Service Found'),
-      );
-    } on FormatException {
-      yield SearchError(
-        error: InvalidFormatException('Invalid Response format'),
-      );
-    } on FetchDataException {
-      yield SearchError(
-        error: NoInternetException('Error During Communication'),
-      );
-    } on BadRequestException {
-      yield SearchError(
-        error: Error400Exception('Invalid Request'),
-      );
-    } on UnauthorisedException {
-      yield SearchError(
-        error: Error400Exception('Unauthorised'),
-      );
-    } on InvalidInputException {
-      yield SearchError(
-        error: Error400Exception('Invalid Input'),
-      );
-    } on InternalServerErrorException {
-      yield SearchError(
-        error: Error500Exception('Internal Server Error'),
-      );
-    } catch (e) {
-      yield SearchError(
-        error: UnknownException(e.toString()),
-      );
-    }
-  }
 }
 
 class SearchNextPageByKeyword extends SearchEvents {
@@ -71,63 +14,9 @@ class SearchNextPageByKeyword extends SearchEvents {
 
   @override
   String toString() => 'SearchNextPageByKeyword { keyword: $keyword }';
-
-  @override
-  Stream<SearchState> run(SearchRepos searchRepos) async* {
-    print(this.toString());
-    try {
-      yield SearchLoadingMore(storyListItemList: storyListItemList);
-      StoryListItemList newStoryListItemList =
-          await searchRepos.searchNextPageByKeyword(keyword);
-      storyListItemList.addAll(newStoryListItemList);
-      yield SearchLoaded(storyListItemList: storyListItemList);
-    } on SocketException {
-      yield SearchError(
-        error: NoInternetException('No Internet'),
-      );
-    } on HttpException {
-      yield SearchError(
-        error: NoServiceFoundException('No Service Found'),
-      );
-    } on FormatException {
-      yield SearchError(
-        error: InvalidFormatException('Invalid Response format'),
-      );
-    } on FetchDataException {
-      yield SearchError(
-        error: NoInternetException('Error During Communication'),
-      );
-    } on BadRequestException {
-      yield SearchError(
-        error: Error400Exception('Invalid Request'),
-      );
-    } on UnauthorisedException {
-      yield SearchError(
-        error: Error400Exception('Unauthorised'),
-      );
-    } on InvalidInputException {
-      yield SearchError(
-        error: Error400Exception('Invalid Input'),
-      );
-    } on InternalServerErrorException {
-      yield SearchError(
-        error: Error500Exception('Internal Server Error'),
-      );
-    } catch (e) {
-      yield SearchError(
-        error: UnknownException(e.toString()),
-      );
-    }
-  }
 }
 
 class ClearKeyword extends SearchEvents {
   @override
   String toString() => 'ClearKeyword';
-
-  @override
-  Stream<SearchState> run(SearchRepos searchRepos) async* {
-    print(this.toString());
-    yield SearchInitState();
-  }
 }
