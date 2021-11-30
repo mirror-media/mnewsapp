4beb8fc636e2f7ae13be790e2dfd616236cba8c1
samfeng0517 <sam0880517@gmail.com>
2021-11-30 17:16:41 +0800

    Move init firebase position

--- lib/helpers/firebaseMessagingHelper.dart
+++ lib/helpers/firebaseMessagingHelper.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:tv/helpers/routeGenerator.dart';
@@ -6,7 +5,6 @@ import 'package:tv/helpers/routeGenerator.dart';
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
-  await Firebase.initializeApp();
   print('Handling a background message ${message.messageId}');
 }
 
