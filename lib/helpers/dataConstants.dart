602728154a41d01dc2e59e6ed85bb34cff0ab6cc
Sam Feng <sam0880517@gmail.com>
2022-04-18 15:11:02 +0800

    feat(asset): add download icon

--- lib/helpers/dataConstants.dart
+++ lib/helpers/dataConstants.dart
@@ -27,6 +27,7 @@ const String logoPng = 'assets/image/logo.png';
 const String hammerSvg = 'assets/image/ombuds/hammer.svg';
 const String adUnitIdJson = 'assets/json/adUnitId.json';
 const String reportSvg = 'assets/image/ombuds/report.svg';
+const String downloadSvg = 'assets/image/download.svg';
 
 /// color
 const Color themeColor = Color(0xff003366);
