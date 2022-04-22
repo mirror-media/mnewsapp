05d70f96004233b6496c75a38e7e2de72c60e89e
Sam Feng <sam0880517@gmail.com>
2022-04-22 14:34:56 +0800

    feat: add app_tracking_transparency for iOS

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -7,6 +10,9 @@ import 'package:tv/mNewsApp.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Environment().initConfig(BuildFlavor.development);
+  if (Platform.isIOS) {
+    await AppTrackingTransparency.requestTrackingAuthorization();
+  }
   await Firebase.initializeApp();
 
   SystemChrome.setPreferredOrientations(
