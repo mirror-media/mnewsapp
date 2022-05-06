7e49bfec578b764945d088f059f8abb9532de545
Sam Feng <sam0880517@gmail.com>
2022-05-06 11:38:52 +0800

    feat(promotionVideoService): check image exist or not

--- lib/services/promotionVideosService.dart
+++ lib/services/promotionVideosService.dart
@@ -4,6 +4,7 @@ import 'package:tv/helpers/environment.dart';
 import 'package:tv/helpers/apiBaseHelper.dart';
 import 'package:tv/models/graphqlBody.dart';
 import 'package:tv/models/youtubePlaylistItem.dart';
+import 'package:http/http.dart' as http;
 
 abstract class PromotionVideosRepos {
   Future<List<YoutubePlaylistItem>> fetchAllPromotionVideos();
@@ -43,8 +44,17 @@ class PromotionVideosServices implements PromotionVideosRepos {
         List<YoutubePlaylistItem>.from(
             jsonResponse['data']['allPromotionVideos'].map((ytVideo) =>
                 YoutubePlaylistItem.fromPromotionVideosJson(ytVideo)));
-    youtubePlaylistItemList
-        .removeWhere((element) => element.name == 'Private video');
+    List<int> removeIndex = [];
+    for (int i = 0; i < youtubePlaylistItemList.length; i++) {
+      final reponse =
+          await http.get(Uri.parse(youtubePlaylistItemList[i].photoUrl));
+      if (reponse.statusCode != 200) {
+        removeIndex.add(i);
+      }
+    }
+    for (var index in removeIndex) {
+      youtubePlaylistItemList.removeAt(index);
+    }
     return youtubePlaylistItemList;
   }
 }
