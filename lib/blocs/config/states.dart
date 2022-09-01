e816321e770ac19b4a4b24d24ba41d9efdebc29e
Sam Feng <sam0880517@gmail.com>
2022-09-01 11:56:49 +0800

    feat(initial): remove unused topics

--- lib/blocs/config/states.dart
+++ lib/blocs/config/states.dart
@@ -1,5 +1,3 @@
-import 'package:tv/models/topic.dart';
-
 abstract class ConfigState {}
 
 class ConfigInitState extends ConfigState {}
@@ -9,12 +7,10 @@ class ConfigLoading extends ConfigState {}
 class ConfigLoaded extends ConfigState {
   final bool isSuccess;
   final String minAppVersion;
-  final List<Topic> topics;
   final String appVersion;
   ConfigLoaded({
     required this.isSuccess,
     required this.minAppVersion,
-    required this.topics,
     required this.appVersion,
   });
 }
