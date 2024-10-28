dd17c2e26bd7a45164fc0d67bfc5b84621333e9d
jnywu <johnnywu203@gmail.com>
2024-10-28 16:37:20 +0800

    Watch App initial

---
+++ ios/mnews Watch App/mnewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mnewsApp.swift
+//  mnews Watch App
+//
+//  Created by 吳求元 on 2024/10/28.
+//
+
+import SwiftUI
+
+@main
+struct mnews_Watch_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
